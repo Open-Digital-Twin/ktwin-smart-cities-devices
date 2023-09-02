dc52ccb0bd704c86cad4781378cdcd60f15ea94d
agwermann <alexandrewermann@gmail.com>
2023-09-02 14:42:41 -0300

    refactor: update sensor data methods

--- pole-device/publisher.py
+++ pole-device/publisher.py
@@ -31,22 +31,55 @@ def load_mqtt_client(config: MQTTClientConfig) -> MQTTClient:
 def build_message(msg_count: int):
     msg = {}
     msg["msg_count"] = msg_count
-    msg["sensor"] = build_sensor_data()
+    msg["sensor"] = build_air_quality_observed()
     return json.dumps(msg)
 
-def build_sensor_data():
+def build_air_quality_observed() -> dict:
     sensor_data = dict()
-    sensor_data["type"] = 1
-    sensor_data["air_temperature"] = 10
-    sensor_data["process_temperature"] = 10
-    sensor_data["rotational_speed"] = 10
-    sensor_data["torque"] = 10
-    sensor_data["tool_wear"] = 5
-    sensor_data["twf"] = 10
-    sensor_data["hdf"] = 10
-    sensor_data["pwf"] = 10
-    sensor_data["osf"] = 10
-    sensor_data["rnf"] = 10
+    sensor_data["reliability"] = 8
+    sensor_data["volatileOrganicCompoundsTotal"] = 0
+    sensor_data["CO2Density"] = 8
+    sensor_data["CODensity"] = 8
+    sensor_data["PM1Density"] = 8
+    sensor_data["PM10Density"] = 8
+    sensor_data["PM25Density"] = 8
+    sensor_data["SO2Density"] = 8
+    sensor_data["C6H6Density"] = 8
+    sensor_data["NIDensity"] = 8
+    sensor_data["ASDensity"] = 8
+    sensor_data["CDDensity"] = 8
+    sensor_data["NO2Density"] = 8
+    sensor_data["O3Density"] = 8
+    sensor_data["PBDensity"] = 8
+    sensor_data["SH2Density"] = 8
+    sensor_data["precipitation"] = 8
+    sensor_data["temperature"] = 8
+    sensor_data["WindDirection"] = 8
+    sensor_data["WindSpeed"] = 8
+    sensor_data["relativeHumidity"] =  8
+    return sensor_data
+
+def build_weather_observed():
+    sensor_data = dict()
+    sensor_data["atmosphericPressure"] = 10
+    sensor_data["dewpoint"] = 8
+    sensor_data["temperature"] = 8
+    sensor_data["illuminance"] = 8
+    sensor_data["precipitation"] = 8
+    sensor_data["relativeHumidity"] = 8
+    sensor_data["snowHeight"] = 8
+    sensor_data["solarRadiation"] = 8
+    sensor_data["streamGauge"] = 8
+    sensor_data["uVIndexMax"] = 8
+    sensor_data["visibility"] = 8
+    sensor_data["WindDirection"] = 8
+    sensor_data["WindSpeed"] = 8
+    return sensor_data
+
+def build_device_observed():
+    sensor_data = dict()
+    sensor_data["batteryLevel"] = 90
+    sensor_data["dateObserved"] = "2023-07-01T01:11:34.39Z"
     return sensor_data
 
 def run():
