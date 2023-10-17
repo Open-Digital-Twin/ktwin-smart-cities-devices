9cc1a144feb65365f48146b832cccc1b3dec1a12
agwermann <alexandrewermann@gmail.com>
2023-10-16 21:34:32 -0300

    add period of messages

--- devices/streetlight-device/publisher.py
+++ devices/streetlight-device/publisher.py
@@ -7,20 +7,30 @@ def build_message(msg_count: int):
     msg = build_streetlight_message(msg_count)
     msg["msg_count"] = msg_count
     msg["timestamp"] = datetime.now().isoformat()
+    print(msg)
     return json.dumps(msg)
 
 def build_streetlight_message(msg_count):
     sensor_data = dict()
-    power_state = ""
-    if msg_count%2:
-        power_state = "on"
-    else:
-        power_state = "off"
-    sensor_data["powerState"] = power_state
+    part_period = int(os.getenv("PART_PERIOD"))
+    full_period = int(os.getenv("FULL_PERIOD"))
+    sensor_data["powerState"] = generate_power_state(msg_count=msg_count, part_period=part_period, full_period=full_period)
     return sensor_data
 
-def run():
+# This method generates status based on the msg_count information
+# 0 -> part_period -> full_period: it changes the power state when message count is equal to part_period, later it changes to a different value when msg_count is full_period.
+def generate_power_state(msg_count, part_period, full_period):
+    if full_period < part_period:
+        raise Exception("full_period must be greater than part_period")
+    
+    if (msg_count % full_period) < part_period:
+        return "off"
+    if (msg_count % full_period) > part_period:
+        return "on"
 
+    return "off"
+
+def run():
     config = load_mqtt_config()
     client = load_mqtt_client(config)
 
