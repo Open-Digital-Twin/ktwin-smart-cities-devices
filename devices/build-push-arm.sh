c183d5d1b7b379aa7f829ad68675d70ce40f3e31
agwermann <alexandrewermann@gmail.com>
2023-10-28 11:03:06 -0300

    ci: create build files for different architectures

---
+++ devices/build-push-arm.sh
@@ -0,0 +1,25 @@
+# Publisher devices
+docker buildx build --platform linux/arm -f publisher.Dockerfile -t ghcr.io/open-digital-twin/ktwin-pole-device-publisher:0.1-arm64 --build-arg DEVICE_NAME=pole-device .
+docker buildx build --platform linux/arm -f publisher.Dockerfile -t ghcr.io/open-digital-twin/ktwin-streetlight-device-publisher:0.1-arm64 --build-arg DEVICE_NAME=streetlight-device .
+docker buildx build --platform linux/arm -f publisher.Dockerfile -t ghcr.io/open-digital-twin/ktwin-parking-spot-device-publisher:0.1-arm64 --build-arg DEVICE_NAME=parking-spot-device .
+docker buildx build --platform linux/arm -f publisher.Dockerfile -t ghcr.io/open-digital-twin/ktwin-ev-charging-device-publisher:0.1-arm64 --build-arg DEVICE_NAME=ev-charging-device .
+docker buildx build --platform linux/arm -f publisher.Dockerfile -t ghcr.io/open-digital-twin/ktwin-battery-device-publisher:0.1-arm64 --build-arg DEVICE_NAME=battery-device .
+
+docker push ghcr.io/open-digital-twin/ktwin-pole-device-publisher:0.1-arm64
+docker push ghcr.io/open-digital-twin/ktwin-streetlight-device-publisher:0.1-arm64
+docker push ghcr.io/open-digital-twin/ktwin-parking-spot-device-publisher:0.1-arm64
+docker push ghcr.io/open-digital-twin/ktwin-ev-charging-device-publisher:0.1-arm64
+docker push ghcr.io/open-digital-twin/ktwin-battery-device-publisher:0.1-arm64
+
+# Subscriber devices
+docker buildx build --platform linux/arm -f subscriber.Dockerfile  -t ghcr.io/open-digital-twin/ktwin-pole-device-subscriber:0.1-arm64 --build-arg DEVICE_NAME=pole-device .
+docker buildx build --platform linux/arm -f subscriber.Dockerfile  -t ghcr.io/open-digital-twin/ktwin-streetlight-device-subscriber:0.1-arm64 --build-arg DEVICE_NAME=streetlight-device .
+docker buildx build --platform linux/arm -f subscriber.Dockerfile  -t ghcr.io/open-digital-twin/ktwin-parking-spot-device-subscriber:0.1-arm64 --build-arg DEVICE_NAME=parking-spot-device .
+docker buildx build --platform linux/arm -f subscriber.Dockerfile  -t ghcr.io/open-digital-twin/ktwin-ev-charging-device-subscriber:0.1-arm64 --build-arg DEVICE_NAME=ev-charging-device .
+docker buildx build --platform linux/arm -f subscriber.Dockerfile  -t ghcr.io/open-digital-twin/ktwin-battery-device-subscriber:0.1-arm64 --build-arg DEVICE_NAME=battery-device .
+
+docker push ghcr.io/open-digital-twin/ktwin-pole-device-subscriber:0.1-arm64
+docker push ghcr.io/open-digital-twin/ktwin-streetlight-device-subscriber:0.1-arm64
+docker push ghcr.io/open-digital-twin/ktwin-parking-spot-device-subscriber:0.1-arm64
+docker push ghcr.io/open-digital-twin/ktwin-ev-charging-device-subscriber:0.1-arm64
+docker push ghcr.io/open-digital-twin/ktwin-battery-device-subscriber:0.1-arm64
