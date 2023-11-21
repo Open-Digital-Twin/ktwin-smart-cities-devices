1a815f9fe6600d248bbc216ff870f4e462e73017
agwermann <alexandrewermann@gmail.com>
2023-11-21 18:01:58 -0300

    refactor: add list of periods

--- devices/streetlight-device/publisher.py
+++ devices/streetlight-device/publisher.py
@@ -2,6 +2,7 @@ import os
 import json
 from datetime import datetime
 from modules.mqtt import load_mqtt_config, load_mqtt_client
+from modules.config import get_period_configs
 
 def build_message(msg_count: int):
     msg = build_streetlight_message(msg_count)
@@ -34,19 +35,12 @@ def run():
     config = load_mqtt_config()
     client = load_mqtt_client(config)
 
-    if os.getenv("N_MESSAGES") == '':
-        n_messages = 3
-    else:
-        n_messages = int(os.getenv("N_MESSAGES"))
+    period_configs = get_period_configs()
 
-    if os.getenv("MESSAGE_PERIOD") == '':
-        message_period = 1
-    else:
-        message_period = float(os.getenv("MESSAGE_PERIOD"))
-
-    client.connect_mqtt()
-    client.publish(n_messages=n_messages, message_period=message_period, callback=build_message)
-    client.disconnect_mqtt()
+    for period in period_configs:
+        client.connect_mqtt()
+        client.publish(n_messages=period.n_messages, message_period=period.message_period, callback=build_message)
+        client.disconnect_mqtt()
 
 if __name__ == '__main__':
     run()
