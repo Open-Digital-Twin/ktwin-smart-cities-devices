c564b32df2c546a438977a52129568c1bbeec1a9
agwermann <alexandrewermann@gmail.com>
2023-11-22 21:27:36 -0300

    fix: fix method name

--- devices/pole-crowd-flow-observed-device/publisher.py
+++ devices/pole-crowd-flow-observed-device/publisher.py
@@ -5,7 +5,7 @@ from modules.mqtt import load_mqtt_config, load_mqtt_client
 from modules.config import get_period_configs
 
 def build_message(msg_count: int):
-    msg = build_crowd_flow_observed()
+    msg = build_crowd_flow_observed(msg_count=msg_count)
     msg["msg_count"] = msg_count
     msg["dateObservedTo"] = datetime.now().isoformat()
     msg["dateObservedFrom"] = datetime.now().isoformat()
