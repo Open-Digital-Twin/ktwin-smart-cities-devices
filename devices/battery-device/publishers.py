2afff052f230c978cac2820f6d1c63d7dfc01694
agwermann <alexandrewermann@gmail.com>
2024-01-09 23:19:42 -0300

    feat: add publishers in battery-device

---
+++ devices/battery-device/publishers.py
@@ -0,0 +1,37 @@
+import os
+import json
+import subprocess
+from dotenv import load_dotenv
+
+
+def load_env(deviceNumber):
+    env = os.environ.copy()
+    env["BROKER_TOPIC"] = os.getenv("BROKER_TOPIC_{}".format(deviceNumber))
+    env["CLIENT_ID"] = os.getenv("CLIENT_ID_{}".format(deviceNumber))
+
+    print(env["BROKER_TOPIC"])
+    print(env["CLIENT_ID"])
+    return env
+
+
+if os.getenv("ENV") == "publishers-local":
+    load_dotenv('publishers-local.env')
+
+NUMBER_DEVICES = int(os.getenv("NUMBER_DEVICES"))
+
+print("Number of devices: " + str(NUMBER_DEVICES))
+
+processes = list()
+
+for i in range(1, NUMBER_DEVICES):
+    command = 'python publisher.py'
+    env = load_env(i)
+    print('Executing command: ' + command)
+
+    process = subprocess.Popen(command, shell=True, env=env)
+    processes.append(process)
+
+for process in processes:
+    process.wait()
+
+print('All processes have finished')
